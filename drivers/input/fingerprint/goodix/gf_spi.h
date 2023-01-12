311111bd977e25b228d0d9fff0d0e63d9b12998b
Peter Cai <peter@typeblog.net>
2023-01-12 12:41:06 +0530

    input: goodix: Disable capacitive key event reporting
    * The goodix fingerprint sensor always sends KEY_HOME to userspace when
      it is touched. This is really annoying because the position of the
      fingerprint scanner and triggers a strange focus change in Android and
      could not be fixed by editing keylayout files.
    * Just disable the whole event reporting. I don't think an extra
      capacitive key at this position (the power button) can do anything
      good.
    * I don't know about how the fpc driver handles this.
    
    Signed-off-by: madmax7896 <madmax78968@gmail.com>

--- drivers/input/fingerprint/goodix/gf_spi.h
+++ drivers/input/fingerprint/goodix/gf_spi.h
@@ -79,21 +79,20 @@ struct gf_ioc_chip_info {
 	unsigned char reserved[5];
 };
 
-#define GF_IOC_MAGIC 'g' /*define magic number*/
-#define GF_IOC_INIT _IOR(GF_IOC_MAGIC, 0, uint8_t)
-#define GF_IOC_EXIT _IO(GF_IOC_MAGIC, 1)
-#define GF_IOC_RESET _IO(GF_IOC_MAGIC, 2)
-#define GF_IOC_ENABLE_IRQ _IO(GF_IOC_MAGIC, 3)
-#define GF_IOC_DISABLE_IRQ _IO(GF_IOC_MAGIC, 4)
-#define GF_IOC_ENABLE_SPI_CLK _IOW(GF_IOC_MAGIC, 5, uint32_t)
-#define GF_IOC_DISABLE_SPI_CLK _IO(GF_IOC_MAGIC, 6)
-#define GF_IOC_ENABLE_POWER _IO(GF_IOC_MAGIC, 7)
-#define GF_IOC_DISABLE_POWER _IO(GF_IOC_MAGIC, 8)
-#define GF_IOC_INPUT_KEY_EVENT _IOW(GF_IOC_MAGIC, 9, struct gf_key)
-#define GF_IOC_ENTER_SLEEP_MODE _IO(GF_IOC_MAGIC, 10)
-#define GF_IOC_GET_FW_INFO _IOR(GF_IOC_MAGIC, 11, uint8_t)
-#define GF_IOC_REMOVE _IO(GF_IOC_MAGIC, 12)
-#define GF_IOC_CHIP_INFO _IOW(GF_IOC_MAGIC, 13, struct gf_ioc_chip_info)
+#define GF_IOC_MAGIC	'g'			/*define magic number*/
+#define GF_IOC_INIT					_IOR(GF_IOC_MAGIC, 0, uint8_t)
+#define GF_IOC_EXIT					_IO(GF_IOC_MAGIC, 1)
+#define GF_IOC_RESET				_IO(GF_IOC_MAGIC, 2)
+#define GF_IOC_ENABLE_IRQ			_IO(GF_IOC_MAGIC, 3)
+#define GF_IOC_DISABLE_IRQ			_IO(GF_IOC_MAGIC, 4)
+#define GF_IOC_ENABLE_SPI_CLK		_IOW(GF_IOC_MAGIC, 5, uint32_t)
+#define GF_IOC_DISABLE_SPI_CLK		_IO(GF_IOC_MAGIC, 6)
+#define GF_IOC_ENABLE_POWER			_IO(GF_IOC_MAGIC, 7)
+#define GF_IOC_DISABLE_POWER		_IO(GF_IOC_MAGIC, 8)
+#define GF_IOC_ENTER_SLEEP_MODE		_IO(GF_IOC_MAGIC, 10)
+#define GF_IOC_GET_FW_INFO			_IOR(GF_IOC_MAGIC, 11, uint8_t)
+#define GF_IOC_REMOVE				_IO(GF_IOC_MAGIC, 12)
+#define GF_IOC_CHIP_INFO			_IOW(GF_IOC_MAGIC, 13, struct gf_ioc_chip_info)
 
 #if defined(SUPPORT_NAV_EVENT)
 #define GF_IOC_NAV_EVENT _IOW(GF_IOC_MAGIC, 14, gf_nav_event_t)
